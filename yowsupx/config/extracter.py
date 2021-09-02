3d88e6e2573826d235eb78d11ea67aeeeea13d79
Kaisar <anjonadmin@163.com>
2021-09-02 17:56:27 +0800

    fix:Extract from local directory no longer dependent on android device

--- yowsupx/config/extracter.py
+++ yowsupx/config/extracter.py
@@ -3,12 +3,13 @@ import json
 import logging
 import os
 import random
-import shlex
 import shutil
-# from hashlib import pbkdf2_hmac
+from hashlib import pbkdf2_hmac
 from typing import Dict
 from xml.etree import ElementTree
 
+from Crypto import Cipher
+from Crypto.Cipher import AES
 from yowsupx.config import (AppNotInstalledException, KeyPairInvalideException,
                             NoRootException)
 from yowsupx.config.adbwrapper import AdbWrapper
@@ -61,7 +62,7 @@ def fromDevice(serial: str = None, package: str = 'com.whatsapp', outdir: str = 
     prefs_light = loadPrefsFromDevice(device, prefs_light_filepath)
 
     # extract config file
-    config_filepath = extractConfig(keystore, prefs_light, device, outdir)
+    config_filepath = extractConfig(keystore, prefs_light, outdir)
     logger.info(f'save to {config_filepath}')
     logger.info('Finish')
     return config_filepath
@@ -71,7 +72,6 @@ def fromDirectory(datadir: str, serial: str = None, package: str = 'com.whatsapp
     """
     Extract the yowsup configuration file from the directory.
     """
-    device = AdbWrapper.connect(serial)
     # extract prefs file
     logger.info('extract config file')
     keystore_filename = 'keystore.xml'
@@ -90,20 +90,20 @@ def fromDirectory(datadir: str, serial: str = None, package: str = 'com.whatsapp
     prefs_light = loadPrefsFromDirectory(prefs_light_filepath)
 
     # extract config file
-    config_filepath = extractConfig(keystore, prefs_light, device, outdir)
+    config_filepath = extractConfig(keystore, prefs_light, outdir)
     logger.info(f'save to {config_filepath}')
     logger.info('Finish')
     return config_filepath
 
 
-def extractConfig(keystore: Dict[str, str], prefs_light: Dict[str, str], device: AdbWrapper, outdir: str):
+def extractConfig(keystore: Dict[str, str], prefs_light: Dict[str, str], outdir: str):
     client_static_keypair = keystore.get('client_static_keypair', '')
     if not client_static_keypair:
         client_static_keypair_encrypted = keystore.get('client_static_keypair_pwd_enc', '')
         if not client_static_keypair_encrypted:
             logger.info('client_static_keypair miss')
             raise KeyPairInvalideException('client_static_keypair not found')
-        client_static_keypair = decryptKeyPair(device, client_static_keypair_encrypted)
+        client_static_keypair = decryptKeyPair(client_static_keypair_encrypted)
     logger.debug('client_static_keypair=' + client_static_keypair)
 
     server_static_public = keystore.get('server_static_public', '')
@@ -157,34 +157,27 @@ def __find_file(name, path):
         if name in files:
             return os.path.join(root, name)
 
-# def decryptKeyPair(self, keypair_enc: str):
-#     array = json.loads(keypair_enc)
-#     type = int(array[0])
-#     ciphertext = base64.b64decode(array[1] + '=' * 3)
-#     iv = base64.b64decode(array[2] + '=' * 3)
-#     salt = base64.b64decode(array[3] + '=' * 3)
-#     password = array[4]
-#     if type != 2:
-#         raise KeyPairInvalideException('KeyPair type shoud be 2')
-#     magic_token = [chr(ord(c) ^ 18) for c in SOMETOKEN]
-#     magic_token = ''.join(magic_token) + password
 
-#     key = pbkdf2_hmac('sha1', magic_token.encode('utf-8'), salt, 16, 32)
-#     from Crypto.Cipher import AES
-#     cipher = AES.new(key, AES.MODE_OFB, iv)
-#     print(base64.b64encode(cipher.decrypt(ciphertext)).decode('utf-8'))
-    # decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationOFB(key, iv), PADDING_NONE)
-    # decryptedData = decrypter.feed(ciphertext)
-    # decryptedData += decrypter.feed()
-    # return base64.b64encode(decryptedData).decode('utf-8')
-
-
-def decryptKeyPair(device: AdbWrapper, keypair_enc: str) -> str:
-    dirpath = os.path.dirname(os.path.abspath(__file__))
-    device_filename = '/data/local/tmp/decrypt.dex'
-    device.push(os.path.join(dirpath, 'common', 'decrypt.dex'), device_filename)
-    keypair_enc = keypair_enc.replace('"', '\\"')
-    return device.shell(f'echo {shlex.quote(keypair_enc)} | CLASSPATH={device_filename} app_process /system/bin  com.kaisar.wakeypairtools.Decrypt').strip()
+def decryptKeyPair(keypair_enc: str):
+    array = json.loads(keypair_enc)
+    type = int(array[0])
+    ciphertext = base64.urlsafe_b64decode(b64padding(array[1]))
+    iv = base64.urlsafe_b64decode(b64padding(array[2]))
+    salt = base64.urlsafe_b64decode(b64padding(array[3]))
+    password = array[4]
+    if type != 2:
+        raise KeyPairInvalideException('KeyPair type shoud be 2')
+    password = (''.join([chr(ord(c) ^ 18) for c in SOMETOKEN]) + password).encode('utf-8')
+    sha1_key = pbkdf2_hmac(
+        hash_name='sha1',
+        password=password,
+        salt=salt,
+        iterations=16,
+        dklen=16
+    )
+    cryptor = AES.new(sha1_key, AES.MODE_OFB, iv)
+    plain_bytes = cryptor.decrypt(ciphertext)
+    return base64.b64encode(plain_bytes).decode('utf-8')
 
 
 def choiceCarrier(country_code: str):
